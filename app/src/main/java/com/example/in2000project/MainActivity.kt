89463a1e2297af7ef17c4f9658346bb10f31408d
Kristin <hkleiros@uio.no>
2022-04-06 14:55:16 +0200

    Removed extra imports, commented colors.xml

--- app/src/main/java/com/example/in2000project/MainActivity.kt
+++ app/src/main/java/com/example/in2000project/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.in2000project
 
-import android.R
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.in2000project.data.*
+import com.example.in2000project.data.AuroraData
 import com.example.in2000project.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.CoroutineScope
