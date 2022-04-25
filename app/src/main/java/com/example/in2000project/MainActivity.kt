b2f3b0ba0ebc45e4f3a8367eed3fb12d503e772b
CM <xcarlmagnusangell@gmail.com>
2022-04-25 13:13:08 +0200

    La til Oscars' graf

--- app/src/main/java/com/example/in2000project/MainActivity.kt
+++ app/src/main/java/com/example/in2000project/MainActivity.kt
@@ -13,12 +13,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import android.graphics.Color
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.LegendEntry
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.IValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.utils.ViewPortHandler
+
+
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val TAG = "MainActivity"
     private val OSLO = "Oslo"
+    lateinit var nordlysvarsel : LineChart
 
     private val viewModel = MainActivityViewModel()
     val scope = CoroutineScope(Dispatchers.IO)
@@ -29,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.loadProbability("Tromsø")
+        //viewModel.loadProbability("Tromsø")
 
         //region UpperMenu
         scope.launch {
@@ -62,6 +77,55 @@ class MainActivity : AppCompatActivity() {
          */
         //endregion
 
+        //region graph forecast
+        //Ansvarlig: Oscar
+
+        nordlysvarsel = findViewById(R.id.Nordlysvarsel)
+
+        val kpLinje = LineDataSet(datavalues1(), "kp verdier")
+        kpLinje.color = Color.CYAN
+        kpLinje.setDrawCircles(false)
+        kpLinje.valueFormatter = KPValueFormater()
+        val skydekeLinje = LineDataSet(datavalues2(), "sky verdier")
+        skydekeLinje.color = Color.BLACK
+        skydekeLinje.setDrawCircles(false)
+        skydekeLinje.valueFormatter = SkyValueFormater()
+        val dataset = ArrayList<ILineDataSet>()
+        dataset.add(kpLinje)
+        dataset.add(skydekeLinje)
+
+        nordlysvarsel.setDrawBorders(true)
+        nordlysvarsel.description = null
+
+        val legend = nordlysvarsel.legend
+        val legendentries = ArrayList<LegendEntry>()
+
+        val legendentryKp = LegendEntry()
+        legendentryKp.formColor = Color.CYAN
+        legendentryKp.label = "KP"
+        legendentries.add(legendentryKp)
+        val legendEntrySky = LegendEntry()
+        legendEntrySky.formColor = Color.BLACK
+        legendEntrySky.label = "Skydekke"
+        legendentries.add(legendEntrySky)
+
+        legend.setCustom(legendentries)
+
+        val yAxisLeft = nordlysvarsel.axisLeft
+        yAxisLeft.valueFormatter = KPAxisFormater()
+        val yAxisRight = nordlysvarsel.axisRight
+        yAxisRight.valueFormatter = SkyAxisFormater()
+        val xAxis = nordlysvarsel.xAxis
+        xAxis.valueFormatter = XAxisFormater()
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        val data = LineData(dataset)
+        nordlysvarsel.data = data
+        nordlysvarsel.invalidate()
+
+        //end region
+
+
         // Setting a onclick listener for the bottom navigation menu.
         val bottomNavigationMenu = findViewById<BottomNavigationView>(
             com.example.in2000project.R.id.bottom_navigation)
@@ -90,4 +154,101 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
+
+    //region functions and classes for graph
+    private fun datavalues1() : ArrayList<Entry> {
+        val punkter = ArrayList<Entry>()
+        punkter.add(Entry(0F, 2F))
+        punkter.add(Entry(1F, 2F))
+        punkter.add(Entry(3F, 1F))
+        punkter.add(Entry(4F, 1F))
+        punkter.add(Entry(5F, 3F))
+        punkter.add(Entry(6F, 4F))
+        punkter.add(Entry(7F, 0F))
+        punkter.add(Entry(8F, 1F))
+        punkter.add(Entry(9F, 1F))
+        punkter.add(Entry(10F, 2F))
+        punkter.add(Entry(11F, 1F))
+        punkter.add(Entry(12F, 0F))
+        punkter.add(Entry(13F, 0F))
+        punkter.add(Entry(14F, 1F))
+        punkter.add(Entry(15F, 2F))
+        punkter.add(Entry(16F, 3F))
+        return punkter
+    }
+    private fun datavalues2() : ArrayList<Entry> {
+        val punkter = ArrayList<Entry>()
+        punkter.add(Entry(0F, 4F))
+        punkter.add(Entry(1F, 7F))
+        punkter.add(Entry(3F, 8F))
+        punkter.add(Entry(4F, 1F))
+        punkter.add(Entry(5F, 2F))
+        punkter.add(Entry(6F, 0F))
+        punkter.add(Entry(7F, 1F))
+        punkter.add(Entry(8F, 3F))
+        punkter.add(Entry(9F, 3F))
+        punkter.add(Entry(10F, 5F))
+        punkter.add(Entry(11F, 7F))
+        punkter.add(Entry(12F, 7F))
+        punkter.add(Entry(13F, 8F))
+        punkter.add(Entry(14F, 9F))
+        punkter.add(Entry(15F, 10F))
+        punkter.add(Entry(16F, 7F))
+        return punkter
+    }
+    private class KPValueFormater : IValueFormatter {
+        override fun getFormattedValue(
+            value: Float,
+            entry: Entry?,
+            dataSetIndex: Int,
+            viewPortHandler: ViewPortHandler?
+        ): String {
+            return value.toInt().toString()
+        }
+
+    }
+    private class SkyValueFormater : IValueFormatter {
+        override fun getFormattedValue(
+            value: Float,
+            entry: Entry?,
+            dataSetIndex: Int,
+            viewPortHandler: ViewPortHandler?
+        ): String {
+            return ((value*10).toInt().toString() + "%")
+        }
+
+    }
+    private class KPAxisFormater : IAxisValueFormatter {
+        override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+            axis?.axisMinimum = 0F
+            return value.toInt().toString()
+        }
+
+    }
+    private class SkyAxisFormater : IAxisValueFormatter {
+        override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+            axis?.axisMinimum = 0F
+            return (value*10).toInt().toString()
+        }
+
+    }
+    private class XAxisFormater : IAxisValueFormatter {
+        override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+            axis?.axisMinimum = 0F
+            axis?.setLabelCount(5, true)
+            when (value) {
+                0F -> return "00:00"
+                4F -> return "12:00"
+                8F -> return "00:00"
+                12F -> return "12:00"
+                16F -> return "00:00"
+
+            }
+            return value.toString()
+        }
+
+    }
+    //end region
+
 }
