3f5bc7e3424e0876ae2c5000cb04521dbb8489af
Julia <juliachm@uio.no>
2022-05-09 23:57:35 +0200

    Fjerne kommentert kode som skapte conflicts

--- app/src/main/java/com/example/in2000project/MainActivity.kt
+++ app/src/main/java/com/example/in2000project/MainActivity.kt
@@ -120,47 +120,6 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-
-        /*
-        viewModel.loadProbability("Tromsø")
-        viewModel.getData().observe(this) {
-            binding.sannsynlighetsView.findViewById<TextView>(R.id.currentTime).text =
-                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
-                    .toString()
-            binding.sannsynlighetsView.findViewById<ImageView>(R.id.weatherImage)
-            binding.sannsynlighetsView.findViewById<TextView>(R.id.northernLight).text =
-                it[0].toString()
-            binding.sannsynlighetsView.findViewById<TextView>(R.id.kpIndex).text =
-                it[4].toString()
-            binding.sannsynlighetsView.findViewById<TextView>(R.id.cloudCoverage).text =
-                it[3].toString() + "%"
-
-            // Checks if there is a high chance of seeing aurora. If yes, show aurora icon
-            // Checks if there are clouds. If yes, shows cloud icon whether it's day or night
-            // Checks if it is night. If yes shows moon icon
-            // Otherwise shows sun icon
-            if (it[0] == "Høy sjanse for å se nordlys") {
-                Glide.with(binding.sannsynlighetsView.findViewById<ImageView>(R.id.weatherImage))
-                    .load(R.drawable.ic_aurora)
-                    .into(binding.sannsynlighetsView.findViewById(R.id.weatherImage))
-            } else if (!viewModel.checkClouds()) {
-                Glide.with(binding.sannsynlighetsView.findViewById<ImageView>(R.id.weatherImage))
-                    .load(R.drawable.ic_cloudy)
-                    .into(binding.sannsynlighetsView.findViewById(R.id.weatherImage))
-            } else if (viewModel.checkSun()) {
-                Glide.with(binding.sannsynlighetsView.findViewById<ImageView>(R.id.weatherImage))
-                    .load(R.drawable.ic_moon)
-                    .into(binding.sannsynlighetsView.findViewById(R.id.weatherImage))
-            } else {
-                Glide.with(binding.sannsynlighetsView.findViewById<ImageView>(R.id.weatherImage))
-                    .load(R.drawable.ic_sunny)
-                    .into(binding.sannsynlighetsView.findViewById(R.id.weatherImage))
-            }
-        }
-
-         */
-
-
         //region graph forecast
         //Ansvarlig: Oscar
 
