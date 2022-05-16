87b85b83069500b047902b6e96cf3bbd54115072
Tobias Nordby <tobnord@gmail.com>
2022-05-16 19:46:07 +0200

    Cleaning up some code in SettingsActivityTest.kt

--- app/src/androidTest/java/com/example/in2000project/SettingsActivityTest.kt
+++ app/src/androidTest/java/com/example/in2000project/SettingsActivityTest.kt
@@ -4,6 +4,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Rule
 import org.junit.Test
@@ -12,16 +13,16 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SettingsActivityTest {
 
+    @get:Rule
+    val activityRule: ActivityScenarioRule<SettingsActivity> = ActivityScenarioRule(SettingsActivity::class.java)
+
     @Test
     fun test_isActivityInView() {
-        val activityScenario = ActivityScenario.launch(SettingsActivity::class.java)
         onView(withId(R.id.settings_activity)).check(matches(isDisplayed()))  // Visibility test method 1
     }
 
     @Test
     fun test_visibility_of_toolbar_and_the_settings() {
-        val activityScenario = ActivityScenario.launch(SettingsActivity::class.java)
-
         // Toolbar visibility check
         onView(withId(R.id.my_toolbar))
             .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))  // Visibility test method 2
