b28c8f4023c6bf6b904a4c1df8c8a746dc221047
Tobias Nordby <tobnord@gmail.com>
2022-05-16 19:02:58 +0200

    Adding more tests to InfoActivity

--- app/src/androidTest/java/com/example/in2000project/InfoActivityTest.kt
+++ app/src/androidTest/java/com/example/in2000project/InfoActivityTest.kt
@@ -1,8 +1,10 @@
 package com.example.in2000project
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Rule
@@ -38,11 +40,12 @@ class InfoActivityTest {
             .check(matches(ViewMatchers.isDisplayed()))
     }
 
-//    @Test
-//    fun test_visibility_textview5() {
-//        onView(ViewMatchers.withId(R.id.textView5))
-//            .check(matches(ViewMatchers.isDisplayed()))
-//    }
+    @Test
+    fun test_visibility_textview3() {
+        onView(ViewMatchers.withId(R.id.textView3))
+            .perform(ViewActions.scrollTo())
+            .check(matches(ViewMatchers.isDisplayed()))
+    }
 
     @Test
     fun test_visibility_imageview() {
@@ -54,5 +57,23 @@ class InfoActivityTest {
     fun test_visibility_bottom_navigation() {
         onView(ViewMatchers.withId(R.id.bottom_navigation))
             .check(matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_text_in_textview() {
+        onView(ViewMatchers.withId(R.id.textView))
+            .check(matches(withText(R.string.Nordlys)))
+    }
+
+    @Test
+    fun test_text_in_textview2() {
+        onView(ViewMatchers.withId(R.id.textView2))
+            .check(matches(withText(R.string.nordlysInfo1)))
+    }
+
+    @Test
+    fun test_text_in_textview3() {
+        onView(ViewMatchers.withId(R.id.textView3))
+            .check(matches(withText(R.string.nordlysInfo2)))
     }
 }
